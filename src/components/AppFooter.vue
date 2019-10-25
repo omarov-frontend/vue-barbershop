09ab85f6fa08b452630bbd005923bdf2061741cb
Shamil Omarov <s_omarov05@mail.ru>
2019-10-25 19:27:24 +0300

    add files

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+     <footer class="ftr">
+        <div class="center-block-main clearfix">
+            <div class="content-contact-block ftr-block-left">
+                <p>БАРБЕРШОП «БОРОДИНСКИЙ»</p>
+                <p>АДРЕС: Г. САНКТ-ПЕТЕРБУРГ, УЛ. Б. КОНЮШЕННАЯ, Д. 19/8</p>
+                <a href="#">КАК НАС НАЙТИ?</a>
+                <p>ТЕЛЕФОН: +7 (495) 666-02-66</p>
+            </div>
+            <div class="ftr-social-block">
+                <p>ДАВАЙТЕ ДРУЖИТЬ!</p>
+                <a href="#" class="btn ftr-vk-social"></a>
+                <a href="#" class="btn ftr-fcbk-social"></a>
+                <a href="#" class="btn ftr-inst-social"></a>
+            </div>
+            <div class="ftr-block-right">
+                <p>РАЗРАБОТАНО:</p>
+                <a href="#" class="btn">html academy</a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+    
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+   <header class="main-header">
+        <div class="center-block-main">
+            <div class="header clearfix">
+                <nav>
+                    <ul class="menu">
+                        <li><a href="#">ИНФОРМАЦИЯ</a></li>
+                        <li><a href="#">НОВОСТИ</a></li>
+                        <li><a href="price.html">ПРАЙС-ЛИСТ</a></li>
+                        <li><a href="item.html">МАГАЗИН</a></li>
+                        <li><a href="#">КОНТАКТЫ</a></li>
+                    </ul>
+                    <div class="input">
+                        <a href="#"><img src="../assets/images/input.png" alt=""><span>ВХОД</span></a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
